7d8e199acd2adec4b5d0aa33125415fbb223d244
彩銘羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2024-02-18 01:40:39 +0800

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE := WordFinder
 
 LOCAL_MODULE_FILENAME := wordfinder
 
-COMMAND := -static -Wl,--strip-all -Os -fPIC -pipe -g0 -flto=thin -Wl,--gc-sections -ffunction-sections -fdata-sections -falign-functions -falign-loops -Wl,--sort-section=alignment -fvisibility=hidden -ffast-math -fno-stack-protector -fno-builtin -fmerge-all-constants -fmerge-constants -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-strict-aliasing -fno-rtti -fno-exceptions -fomit-frame-pointer -fno-stack-check -fms-extensions -fdeclspec -fexceptions
+COMMAND := -static -Wl,--strip-all -Os -fPIC -pipe -g0 -flto=thin -Wl,--gc-sections -ffunction-sections -fdata-sections -falign-functions -falign-loops -Wl,--sort-section=alignment -fvisibility=hidden -ffast-math -fno-stack-protector -fno-builtin -fmerge-all-constants -fmerge-constants -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-strict-aliasing -fno-rtti -fno-exceptions -fomit-frame-pointer -fno-stack-check -fms-extensions -fdeclspec -fvisibility-inlines-hidden
 
 LOCAL_CFLAGS := $(COMMAND)
 LOCAL_CPPFLAGS := -std=c++17
